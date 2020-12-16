d359981f3d9ec61333079b68723f1ac0bf1498a3
JaiminPatel <jaimin.patel@oneadvanced.com>
2020-12-16 17:54:37 +0530

    fix: updating jest test

--- deploy.js
+++ deploy.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch')
 const deploy = async (schema, apiKey) => {
-  const url = 'https://data.oneadvanced.io/control/v1/deployment' // 'https://data.svc.oneadvanced.com/control/v1/apps/deploy' // MD deployment url
+  const url = 'https://data.oneadvanced.io/control/v1/deployment'
+  apiKey = apiKey === undefined ? require('minimist')(process.argv.slice(2)).apikey : apiKey
   const res = await fetch(url, {
     method: 'post',
     body: JSON.stringify(schema),
@@ -8,7 +9,7 @@ const deploy = async (schema, apiKey) => {
   })
   const json = await res.json()
   return {
-    statusCode: res.ok ? 200 : res.statusCode,
+    statusCode: res.ok ? 200 : res.status,
     response: json
   }
 }
