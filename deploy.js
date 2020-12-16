ece342df3a01cdb2a96651db8a5c659f81d82b1c
JaiminPatel <jaimin.patel@oneadvanced.com>
2020-12-16 14:24:39 +0530

    feat: adding branding to git action

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
 const deploy = async (schema, apiKey) => {
-  const url = 'https://data.oneadvanced.io/control/v1/apps/deploy' // 'https://data.svc.oneadvanced.com/control/v1/apps/deploy' // MD deployment url
+  const url = 'https://data.oneadvanced.io/control/v1/deployment' // 'https://data.svc.oneadvanced.com/control/v1/apps/deploy' // MD deployment url
   const res = await fetch(url, {
     method: 'post',
     body: JSON.stringify(schema),
