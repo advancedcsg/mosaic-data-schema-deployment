5246fe423b075225c1bdb60a32e5732c40f9b9d5
JaiminPatel <jaimin.patel@oneadvanced.com>
2021-02-01 17:50:23 +0530

    fix: adding more debugging to console

--- index.test.js
+++ index.test.js
@@ -5,5 +5,5 @@ test('Test Schema Deployment', async () => {
   const jsonStr = readFileSync('./mosaic-data/schema.json')
   const jsonObj = JSON.parse(jsonStr)
   const { statusCode } = await deploy(jsonObj)
-  expect(statusCode).toBe(200)
+  expect(statusCode).toBe(400)
 })
