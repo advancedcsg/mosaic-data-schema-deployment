40743b9c81c5f7fdd4a528edbccb90afe051f988
JaiminPatel <jaimin.patel@oneadvanced.com>
2021-02-01 17:49:12 +0530

    fix: adding more debugging to console

--- deploy.js
+++ deploy.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch')
 const deploy = async (schema, apiKey, info) => {
   const url = 'https://data.svc.oneadvanced.com/control/v1/deployment'
   apiKey = apiKey === undefined ? require('minimist')(process.argv.slice(2)).apikey : apiKey
-  info(`Hitting URL : ${url}`)
+  console.log(`Hitting URL : ${url}`)
   const res = await fetch(url, {
     method: 'post',
     body: JSON.stringify(schema),
